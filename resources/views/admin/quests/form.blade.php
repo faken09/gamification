261d9755f412bba4863013af6570bf0a5ef89efc
Sebastian Aa. Jakobsen <seb_jakobsen@live.dk>
2018-04-02 14:10:56 +0200

    test

--- resources/views/admin/quests/form.blade.php
+++ resources/views/admin/quests/form.blade.php
@@ -111,7 +111,7 @@
 
 <div>
     <label>Fjende
-        @if ($errors->has('enemy'))
+        @if ($errors->has('enemy')) test
             <span class="span-error">
                     :{{ $errors->first('enemy') }}
                                     </span>
